a89a23a43dd01678f42d4e70424fb9056fcb6af3
Joey Aghion <joey@aghion.com>
2018-11-15 13:34:05 -0500

    Add gravatars

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,15 @@
 module ProjectsHelper
+  def gravatar_from_log_line(line)
+    gravatar_from_email(email_from_log_line(line))
+  end
+
+  def email_from_log_line(line)
+    line.match(/, ([^ ]*)\)$/)&.[](1)
+  end
+
+  def gravatar_from_email(email)
+    return if email.blank?
+    hash = Digest::MD5.hexdigest(email.downcase)
+    image_tag "https://www.gravatar.com/avatar/#{hash}", class: 'avatar'
+  end
 end
