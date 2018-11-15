56a1a450756c8a4014918ae28b1c5827e5b8a60d
Joey Aghion <joey@aghion.com>
2018-11-15 10:41:33 -0500

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with(
+    name: Horizon.config[:basic_auth_user],
+    password: Horizon.config[:basic_auth_pass]
+  ) if Horizon.config[:basic_auth_pass].present?
 end
---
+++ config/initializers/_config.rb
@@ -0,0 +1,6 @@
+Horizon.mattr_accessor :config
+
+Horizon.config = {
+  basic_auth_user: ENV['BASIC_AUTH_USER'] || 'admin',
+  basic_auth_pass: ENV['BASIC_AUTH_PASS']
+}
