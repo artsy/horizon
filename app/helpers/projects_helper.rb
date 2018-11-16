6a95005842f600f6fe3a34676bcb1f6c89c55085
Joey Aghion <joey@aghion.com>
2018-11-16 11:55:52 -0500

    Improve main page formatting; auto-link github commits

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,4 +1,33 @@
 module ProjectsHelper
+  LOG_LINE_EXPR = /^(?<sha>[0-9a-f]+) (?<date>[0-9\-]+) (?<message>.*) \((?<name>.*), (?<email>.*)\)\w*$/ # %h %ad %s (%an, %ae)
+  GITHUB_REMOTE_EXPR = /https:\/\/github.com\/(?<org>[^\/]+)\/(?<project>[^\.]+).git/
+  STATUS_ICONS = {
+    unknown: '&#10067;',
+    warning: '&#10071;',
+    released: '&check;'
+  }
+
+  def render_log_line(project, line)
+    github_match = project.stages.ordered.first&.git_remote&.match(GITHUB_REMOTE_EXPR)
+    return line unless github_match
+    line_match = line.match(LOG_LINE_EXPR)
+    link = link_to line_match[:sha], "https://github.com/#{github_match[:org]}/#{github_match[:project]}/commit/#{line_match[:sha]}"
+    h(line).sub(Regexp.new(line_match[:sha]), link).html_safe
+  end
+
+  def project_status_icon(project)
+    status = get_status(project)
+    content_tag(:div, class: "project_status project_status_#{status}") do
+      raw(STATUS_ICONS[status])
+    end
+  end
+
+  def get_status(project)
+    return :unknown if project.snapshot.nil?
+    return :released if project.snapshot&.comparisons&.all?(&:released?)
+    :warning
+  end
+
   def gravatar_from_log_line(line)
     gravatar_from_email(email_from_log_line(line))
   end
--- app/models/stage.rb
+++ app/models/stage.rb
@@ -3,4 +3,6 @@ class Stage < ApplicationRecord
   belongs_to :profile, optional: true
 
   acts_as_list scope: :project
+
+  scope :ordered, ->{ order('position ASC') }
 end
