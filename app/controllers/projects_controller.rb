8b02afca2db80c97917c5598201c6dda88628268
Orta Therox <orta.therox@gmail.com>
2018-12-04 12:14:56 -0500

    Adds a view mode for putting on Artsy's dashboard screens

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,18 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
   def index
     @organization_id = projects_params[:organization_id]
     @projects = Project.where(projects_params).sort_by do |project|
       [project.fully_released? ? 1 : 0, project.name]
     end
+  end
+
+  def simple
+    @organization_id = params[:organization_id]
+    @projects = Project.where(projects_params).to_a
+    @released = @projects.select(&:fully_released?).sort_by(&:name)
+    @needs_work = @projects.reject(&:fully_released?).sort_by { |p| p.snapshots.length }
   end
 
   private
