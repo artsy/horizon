aeb2c43b6b3ad72caed6077ec734c9afb488ce39
Joey Aghion <joey@aghion.com>
2019-03-06 12:15:43 -0500

    Allow editing of jsonb field

---
+++ app/models/concerns/jsonb_editable.rb
@@ -0,0 +1,25 @@
+# Allows editing (e.g., via ActiveAdmin) of jsonb columns. Use like:
+#
+# class SomeModel
+#   include JsonbEditable
+#   jsonb_editable :column
+# end
+#
+# Then, update ActiveAdmin definitions to employ the "column_input"
+# getter/setter instead of the "column" field.
+#
+module JsonbEditable
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def jsonb_editable(field_name, as: "#{field_name}_input")
+      define_method("#{as}=") do |val|
+        self.send("#{field_name}=", val.presence && JSON.parse(val))
+      end
+
+      define_method(as) do
+        send(field_name)&.to_json
+      end
+    end
+  end
+end
