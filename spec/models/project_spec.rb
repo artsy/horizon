8c7285f91e35bc8e73e88bafff5391e711de40b6
Joey Aghion <joey@aghion.com>
2019-05-15 12:59:34 -0400

    Avoid violating foreign key constraints when projects are deleted

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Organization, type: :model do
+RSpec.describe Project, type: :model do
   it 'can be deleted despite associations' do
     org = Organization.create!(name: 'artsy')
     project = org.projects.create!(name: 'eigen')
     stages = (1..2).map {|i| project.stages.create!(name: "stage #{i}", git_remote: 'https://github.com/artsy/eigen.git') }
     snapshot = project.snapshots.create!
     comparison = snapshot.comparisons.create!(ahead_stage: stages.first, behind_stage: stages.last)
-    expect(org.destroy).to be_present
+    project.update(snapshot: snapshot)
+    expect(project.destroy).to be_present
   end
 end
