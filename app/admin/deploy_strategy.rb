b83466e02287d2a3909f51b5e7c13571ecf7b708
Joey Aghion <joey@aghion.com>
2019-08-08 14:59:27 -0400

    ActiveAdmin allows creating and editing deploy strategies (w/Eve)

--- app/admin/deploy_strategy.rb
+++ app/admin/deploy_strategy.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Profile do
+ActiveAdmin.register DeployStrategy do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -11,18 +11,19 @@ ActiveAdmin.register Profile do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  belongs_to :stage
 
-  permit_params :name, :organization_id, :basic_username, :basic_password, :environment_input
+  permit_params :provider, :automatic, :profile_id, :arguments_input
 
   form do |f|
     f.semantic_errors *f.object.errors.keys
     f.inputs do
-        f.input :name
-        f.input :organization
-        f.input :basic_username
-        f.input :basic_password
-        f.input :environment_input, as: :text, label: 'Environment'
+        f.input :provider, as: :select, collection: DeployStrategy::PROVIDERS
+        f.input :profile
+        f.input :automatic
+        f.input :arguments_input, label: 'Arguments (JSON)'
     end
     f.actions
   end
+
 end
