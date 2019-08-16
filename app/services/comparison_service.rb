b837f566baddc9aaa104aabb658a575fa71277de
Joey Aghion <joey@aghion.com>
2019-08-16 13:56:16 -0400

    Refactor comparison-related tests into their own file

--- app/services/comparison_service.rb
+++ app/services/comparison_service.rb
@@ -54,7 +54,7 @@ class ComparisonService
       new_snapshot = store_new_snapshot!(project, result, refreshed_at)
       clean_up_old_snapshots
     end
-    unless project.deploy_blocks.unresolved.any?
+    if project.deploy_blocks.unresolved.empty?
       project.stages.select { |s| warrants_deploy?(s) }.each do |stage|
         stage.deploy_strategies.each do |strategy|
           DeployService.start(strategy) if strategy.automatic?
