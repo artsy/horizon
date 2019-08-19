d1fd50eaf187175187befc8290d657aaf2e38d8c
Joey Aghion <joey@aghion.com>
2019-08-19 17:22:21 -0400

    Fix link helper and add test that renders a few admin views (#44)

---
+++ spec/features/admin_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "Administration", type: :feature do
+  scenario 'sets up org and project' do
+    visit '/admin'
+    expect(page).to have_content('Horizon')
+
+    click_link 'Organizations'
+    expect(page).to have_content('There are no Organizations')
+
+    click_link 'New Organization'
+    fill_in 'Name', with: 'Etsy'
+    click_button 'Create Organization'
+    expect(page).to have_content('Etsy')
+
+    click_link 'Projects'
+    expect(page).to have_content('There are no Projects')
+
+    click_link 'New Project'
+    select 'Etsy', from: 'Organization'
+    fill_in 'Name', with: 'Shipping'
+    click_button 'Create Project'
+    expect(page).to have_content('Shipping')
+  end
+end
