7d37812ffa52d381d754358d587d182f8a11658b
Joey Aghion <joey@aghion.com>
2019-08-30 18:05:40 -0400

    Broadcast over websockets when deploy blocks change

--- app/models/deploy_block.rb
+++ app/models/deploy_block.rb
@@ -3,4 +3,14 @@ class DeployBlock < ApplicationRecord
 
   scope :unresolved, -> { where(resolved_at: nil) }
   scope :resolved, -> { where.not(resolved_at: nil) }
+
+  after_save :broadcast_updates
+
+  private
+
+  def broadcast_updates
+    return unless id_previously_changed? || resolved_at_previously_changed?
+    ActionCable.server.broadcast(ProjectChannel.channel_name(project.organization_id), updatedBlocks: true)
+    ActionCable.server.broadcast(ProjectChannel.channel_name, updatedBlocks: true)
+  end
 end
