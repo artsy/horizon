2d65338f7c2fe42a4668a9ef89b957727545b994
Chung-Yi Chi <chung-yi@artsymail.com>
2020-07-19 10:04:14 -0400

    Wait for content loaded to subscribe to project channel
    since we need the organization id rendered in the DOM.

--- app/javascript/channels/project_channel.ts
+++ app/javascript/channels/project_channel.ts
@@ -1,24 +1,26 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create(
-  {
-    channel: "ProjectChannel",
-    organization_id: document.querySelector(
-      "#organization_subscription_identifier",
-    ).value,
-  },
-  {
-    connected() {
-      // Called when the subscription is ready for use on the server
+document.addEventListener("DOMContentLoaded", () => {
+  consumer.subscriptions.create(
+    {
+      channel: "ProjectChannel",
+      organization_id: document.querySelector(
+        "#organization_subscription_identifier",
+      ).value,
     },
-    disconnected() {
-      // Called when the subscription has been terminated by the server
+    {
+      connected() {
+        // Called when the subscription is ready for use on the server
+      },
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
+      received(data) {
+        // Called when there's incoming data on the websocket for this channel
+        if (data.newSnapshots || data.updatedBlocks) {
+          location.reload()
+        }
+      },
     },
-    received(data) {
-      // Called when there's incoming data on the websocket for this channel
-      if (data.newSnapshots || data.updatedBlocks) {
-        location.reload()
-      }
-    },
-  },
-)
+  )
+})
