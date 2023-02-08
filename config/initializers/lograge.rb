1744d33b785262020e3b899a48add5beb0936471
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2023-02-08 17:45:55 -0500

    feat: lograge options use safe navigation when accessing request_id (#560)

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.lograge.custom_options = lambda do |event|
     {
-      request_id: event.payload[:headers]['action_dispatch.request_id']
+      request_id: event.payload[:headers]&.[]('action_dispatch.request_id')
     }.compact
   end
 end
